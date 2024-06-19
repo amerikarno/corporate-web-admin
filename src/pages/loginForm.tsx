1b59ba01f376208ca03abe29647171208951f517
karnake.r-at-549007798570 <karnakeruengchaicharnkij@karnakes-MacBook-Pro.local>
2024-06-19 14:43:30 +0700

    add login

--- src/pages/loginForm.tsx
+++ src/pages/loginForm.tsx
@@ -1,9 +1,12 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import Input from "../component/Input";
 import CryptoJs from "crypto-js";
+import axios from "axios";
 const LoginForm = () => {
   const userRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
+  const [error, setError] = useState(null);
+  const [accessToken, setAccessToken] = useState(null);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -14,15 +17,41 @@ const LoginForm = () => {
       const hashedPassword = CryptoJs.SHA256(password).toString();
       console.log(hashedUsername);
       console.log(hashedPassword);
+
+      axios
+        .post(
+          "http://localhost:1323/admin/v2/login",
+          {
+            hashedUsername: `${hashedUsername}`,
+            hashedPassword: `${hashedPassword}`,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        )
+        .then((res) => {
+          setAccessToken(res.data.accessToken);
+          console.log(res);
+        })
+        .catch((err) => {
+          setError(err.message);
+        });
+      console.log(accessToken);
     }
   };
   return (
     <>
-      <form className="flex flex-col space-y-4" onSubmit={handleSubmit}>
-        <Input title="Email" name="email" ref={userRef} />
-        <Input title="Password" name="password" ref={passwordRef} />
-        <button>Sign in</button>
-      </form>
+      {error ? (
+        <p>{error}</p>
+      ) : (
+        <form className="flex flex-col space-y-4" onSubmit={handleSubmit}>
+          <Input title="Email" name="email" ref={userRef} />
+          <Input title="Password" name="password" ref={passwordRef} />
+          <button>Sign in</button>
+        </form>
+      )}
     </>
   );
 };
