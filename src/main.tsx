df3996962316dccfc0d1e3b588bda1d56d91be17
karnake.r-at-549007798570 <karnakeruengchaicharnkij@karnakes-MacBook-Pro-2.local>
2024-06-29 17:11:53 +0700

    remove unused code

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,7 @@
-import { Fragment } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.scss";
-import { RouteData } from "./common/routingdata";
 import { HelmetProvider } from "react-helmet-async";
-
-import App from "./layout/App";
-
-import Authenticationlayout from "./layout/authenticationlayout";
-
-import Error404 from "./component/Authentication/errorpage/error404/error404";
-
-import ScrollToTop from "./ScrollToTop/ScrolltoTop";
 import Login from "./pages/login";
 import React from "react";
 import Layout from "./component/Layout";
