6e48460b1168549e1fbd74982387431c78875316
karn-at-549007798570 <karnakeruengchaicharnkij@karnakes-MacBook-Pro.local>
2024-07-03 14:04:58 +0700

    fix bugs

--- src/main.tsx
+++ src/main.tsx
@@ -16,6 +16,7 @@ const root = createRoot(container!);
  - disable react dev tools
  - not set state for password by map specific response data
  - send data in JWT token, jwt-decode lib
+ add
 */
 root.render(
   <>
