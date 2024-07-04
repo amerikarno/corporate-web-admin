397f71b87a1ccf46703354b458b8cca5395a64e2
karn-at-549007798570 <karnakeruengchaicharnkij@karnakes-MacBook-Pro.local>
2024-07-04 08:45:29 +0700

    example

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,5 +1,10 @@
 const Welcome = () => {
-  return <div>Welcome to Finansia Digital Asset Admin page</div>;
+  return (
+    <div className="flex flex-col">
+      <p>Welcome to Finansia Digital Asset Admin page</p>
+      <p>Edit by karn</p>
+    </div>
+  );
 };
 
 export default Welcome;
