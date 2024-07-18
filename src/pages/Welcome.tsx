372182e20d699c000947d3c7dd247a2eb661c4ad
karn-at-549007798570 <karnakeruengchaicharnkij@karnakes-MacBook-Pro.local>
2024-07-18 10:39:20 +0700

    pull 20240718

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,9 +1,18 @@
+import Grid2 from "@/components/Grid2";
+import { Input } from "@/components/Input";
+
 const Welcome = () => {
   return (
     <div className="flex flex-col">
       <p>Welcome to Finansia Digital Asset Admin page</p>
       <p>Edit by karn</p>
       <p>10.0</p>
+      <Grid2>
+        <Input label="Title" id="title" name="title" />
+        <br />
+        <Input label="Name" id="name" name="name" />
+        <Input id="surname" label="Surname" name="surname" />
+      </Grid2>
     </div>
   );
 };
