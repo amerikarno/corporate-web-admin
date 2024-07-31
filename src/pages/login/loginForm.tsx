73338ba8f58b3e90351a25f2ec7f06ec33e0fc3d
NotePrin <kunprin.note@gmail.com>
2024-07-31 08:57:03 +0700

    merge prin

--- src/pages/login/loginForm.tsx
+++ src/pages/login/loginForm.tsx
@@ -13,7 +13,6 @@ import { setCookies } from "@/lib/Cookies";
 import axios from "@/api/axios";
 
 const LoginForm = () => {
-  const navigate = useNavigate();
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
