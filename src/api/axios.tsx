eeb507795d8df3f197e2d276aa9962f521eccc64
karn-at-549007798570 <karnakeruengchaicharnkij@karnakes-MacBook-Pro.local>
2024-08-13 08:27:19 +0700

    pull 20240813

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -1,14 +1,9 @@
 import axios from "axios";
 import { log } from "console";
 // import log from "@/log/log";
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-let BASE_URL = import.meta.env.BASE_URL
-// log.info("BASE_URL", BASE_URL)
-log("BASE_URL", BASE_URL)
-if (BASE_URL) {
-  BASE_URL = "http://cwa-api-alb-400970075.eu-north-1.elb.amazonaws.com";
-  // BASE_URL = "http://localhost:1323";
-}
+log("base url:", BASE_URL)
 
 export default axios.create({
   baseURL: BASE_URL,
