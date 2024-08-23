5f3cc8c44fc72dbdb152b5a4af962de25cccc7a4
NotePrin <kunprin.note@gmail.com>
2024-08-23 08:52:30 +0700

    merge getter

--- src/pages/createJob/addIndividualAccount/addIndividualAccount.tsx
+++ src/pages/createJob/addIndividualAccount/addIndividualAccount.tsx
@@ -1,6 +1,15 @@
 import { isAllowedPage } from "@/lib/utils";
 import UnAuthorize from "@/pages/unAuthorizePage/unAuthorize";
 import BlinkDetection from "./livenessOcr/livenessOcr";
+import { Card, CardContent } from "@/components/ui/card";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  TIndividualAccount,
+  individualAccountSchema,
+} from "./constant/schemas";
+import { Input } from "@/components/Input";
+import { Button } from "@/components/ui/button";
 
 export default function AddIndividualAccount() {
   if (!isAllowedPage(2002)) {
--- src/pages/createJob/addIndividualAccount/livenessOcr/livenessOcr.tsx
+++ src/pages/createJob/addIndividualAccount/livenessOcr/livenessOcr.tsx
@@ -15,7 +15,7 @@ const BlinkDetection: React.FC = () => {
 
   useEffect(() => {
     const loadModels = async () => {
-      const MODEL_URL = "/models"; // Update with your model URL
+      const MODEL_URL = "/models";
       await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL);
       await faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL);
       await faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL);
