280a64c7fd8309a4dff3a58f0555fd02f213c10f
Nanon Buaphet <ter4744411@gmail.com>
2024-08-30 12:22:49 +0700

    registeredaddress className changed

--- src/pages/createJob/addIndividualAccount/basicInfo/basicInfo.tsx
+++ src/pages/createJob/addIndividualAccount/basicInfo/basicInfo.tsx
@@ -143,9 +143,7 @@ export default function BasicInfo() {
                     label="Address Number"
                     id="addressNoIDCard"
                     {...register("registeredAddress.homeNumber")}
-                    className={errors.registeredAddress?.homeNumber ?
-                      "block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-red-500 border-2 appearance-none dark:text-white dark:border-gray-500 dark:focus:border-gray-500 focus:outline-none focus:ring-0 focus:border-red-500 peer"
-                        :"block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border border-gray-600 appearance-none dark:text-white dark:border-gray-500 dark:focus:border-gray-500 focus:outline-none focus:ring-0 focus:border-gray-600 peer"
+                    className={"block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border border-gray-600 appearance-none dark:text-white dark:border-gray-500 dark:focus:border-gray-500 focus:outline-none focus:ring-0 focus:border-gray-600 peer"
                     }
                   />
                 </div>
