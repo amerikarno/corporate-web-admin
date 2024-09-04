a963ca9cfd15b4f6c555e70f4a6bcb6ce479bb3e
NotePrin <kunprin.note@gmail.com>
2024-09-04 08:53:00 +0700

    more test create corporate

--- src/__Test__/createJob/addedCorporateAccount/addedCorporateAccount.test.tsx
+++ src/__Test__/createJob/addedCorporateAccount/addedCorporateAccount.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { Provider } from "react-redux";
 import { setUser } from "@/features/user/userSlice";
@@ -40,5 +40,79 @@ describe("addedCorporateAccount", () => {
 
     const instructionText = screen.getByText(/instructions/i);
     expect(instructionText).toBeTruthy();
+
+    const juristicInvestorName = screen.getByLabelText(
+      /juristic investor name/i
+    );
+    const comercialRegisterationNo = screen.getByLabelText(
+      /Commercial Registration No./i
+    );
+    const taxId = screen.getByLabelText(/tax id/i);
+    const dateOfIncorporation = screen.getByLabelText(/date of incorporation/i);
+    const registerAddressNumber = screen.getByTestId(/registeredBusiness-0/i);
+    const registerTambon = screen.getByTestId(/registeredBusiness-6/i);
+    const registerAmphoe = screen.getByTestId(/registeredBusiness-7/i);
+    const registerProvince = screen.getByTestId(/registeredBusiness-8/i);
+    const registerZipCode = screen.getByTestId(/registeredBusiness-9/i);
+    const registerCountry = screen.getByTestId(/registeredBusiness-10/i);
+    const incorporatePlaceAddressNumber = screen.getByTestId(
+      /placeofIncorporation-0/i
+    );
+    const incorporatePlaceTambon = screen.getByTestId(
+      /placeofIncorporation-6/i
+    );
+    const incorporatePlaceAmphoe = screen.getByTestId(
+      /placeofIncorporation-7/i
+    );
+    const incorporatePlaceProvince = screen.getByTestId(
+      /placeofIncorporation-8/i
+    );
+    const incorporatePlaceZipCode = screen.getByTestId(
+      /placeofIncorporation-9/i
+    );
+    const incorporatePlaceCountry = screen.getByTestId(
+      /placeofIncorporation-10/i
+    );
+
+    fireEvent.change(juristicInvestorName, { target: { value: "hello" } });
+    expect(juristicInvestorName).toHaveValue("hello");
+    fireEvent.change(comercialRegisterationNo, { target: { value: "1234" } });
+    expect(comercialRegisterationNo).toHaveValue("1234");
+    fireEvent.change(taxId, { target: { value: "123456789" } });
+    expect(taxId).toHaveValue("123456789");
+    fireEvent.change(dateOfIncorporation, { target: { value: "1988-11-12" } });
+    expect(dateOfIncorporation).toHaveValue("1988-11-12");
+    fireEvent.change(registerAddressNumber, { target: { value: "123" } });
+    expect(registerAddressNumber).toHaveValue("123");
+    fireEvent.change(registerTambon, { target: { value: "tambon" } });
+    expect(registerTambon).toHaveValue("tambon");
+    fireEvent.change(registerAmphoe, { target: { value: "amphoe" } });
+    expect(registerAmphoe).toHaveValue("amphoe");
+    fireEvent.change(registerProvince, { target: { value: "province" } });
+    expect(registerProvince).toHaveValue("province");
+    fireEvent.change(registerZipCode, { target: { value: 12345 } });
+    expect(registerZipCode).toHaveValue(12345);
+    fireEvent.change(registerCountry, { target: { value: "country" } });
+    expect(registerCountry).toHaveValue("country");
+    fireEvent.change(incorporatePlaceAddressNumber, {
+      target: { value: "777" },
+    });
+    expect(incorporatePlaceAddressNumber).toHaveValue("777");
+    fireEvent.change(incorporatePlaceTambon, { target: { value: "tambon" } });
+    expect(incorporatePlaceTambon).toHaveValue("tambon");
+    fireEvent.change(incorporatePlaceAmphoe, { target: { value: "amphoe" } });
+    expect(incorporatePlaceAmphoe).toHaveValue("amphoe");
+    fireEvent.change(incorporatePlaceProvince, {
+      target: { value: "province" },
+    });
+    expect(incorporatePlaceProvince).toHaveValue("province");
+    fireEvent.change(incorporatePlaceZipCode, { target: { value: 12345 } });
+    expect(incorporatePlaceZipCode).toHaveValue(12345);
+    fireEvent.change(incorporatePlaceCountry, { target: { value: "country" } });
+    expect(incorporatePlaceCountry).toHaveValue("country");
+
+    // const nextBtn = screen.getByText(/next form/i);
+    // fireEvent.click(nextBtn);
+    // expect(instructionText).not.toBeTruthy();
   });
 });
