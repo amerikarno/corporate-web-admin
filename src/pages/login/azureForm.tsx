76aef5e5bfba82e963cba2cfba714f6da6deca33
NotePrin <kunprin.note@gmail.com>
2024-09-10 10:20:31 +0700

    mock login

--- src/pages/login/azureForm.tsx
+++ src/pages/login/azureForm.tsx
@@ -1,19 +1,21 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import api from "@/api/axios";
 import { Button } from "@/components/ui/button";
-import { useLocation } from "react-router-dom";
-import { Input } from "@/components/Input";
+import { useLocation, useNavigate } from "react-router-dom";
+import { setCookies } from "@/lib/Cookies";
+import { setToken } from "@/features/authen/authenSlice";
+import { useDispatch } from "react-redux";
+import { setUser, TUser } from "@/features/user/userSlice";
+import { jwtDecode } from "jwt-decode";
+import { clearCorporateData } from "@/features/editCorporateData/editCorporateData";
 
 const AzureForm: React.FC = () => {
   const [accessToken, setAccessToken] = useState<string | null>(null);
   const [userData, setUserData] = useState<any>(null);
   const location = useLocation();
-  let emailAzure = useRef<string>("");
-
-  const handleAzure = (e: React.ChangeEvent<HTMLInputElement>) => {
-    emailAzure.current = e.target.value;
-    console.log(emailAzure.current);
-  };
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   useEffect(() => {
     // ดึง access_token จาก URL query parameters
@@ -26,17 +28,60 @@ const AzureForm: React.FC = () => {
     }
   }, [location.search]);
 
-  const handleLogin = async () => {
-    // window.location.href = "http://localhost/api/v1/authen/login/azure";
-    await axios
-      .post("http://localhost:1323/api/v1/authen/login/azure", {})
-      .then((response) => {
-        console.log("User Data:", response.data);
-        setUserData(response.data);
+  const mockLogin = async () => {
+    const hashedUsername =
+      "f0179dd3d8f5f85dd303326911320bd3f985ac4cb89d6b3a44b30f0f36249a75";
+    const hashedPassword =
+      "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8";
+
+    api
+      .post(
+        "/api/v1/authen/login",
+        {
+          hashedUsername: `${hashedUsername}`,
+          hashedPassword: `${hashedPassword}`,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      )
+      .then((res) => {
+        console.log(res);
+        dispatch(setToken(res.data.accessToken));
+        setCookies(res.data.accessToken);
+        const user: TUser = jwtDecode(res.data.accessToken);
+        localStorage.clear();
+        dispatch(clearCorporateData());
+        dispatch(setUser(user));
+        navigate("/");
       })
-      .catch((error) => {
-        console.error("Error fetching user data:", error);
+      .catch((err) => {
+        console.log(err);
       });
+  };
+
+  const handleLogin = async () => {
+    // mockLogin();
+
+    // window.location.href = "http://localhost/api/v1/authen/login/azure";
+    try {
+      const res = await api.post(
+        "/api/v1/authen/login/azure",
+        {},
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      );
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   // useEffect(() => {
