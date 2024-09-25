f3fd00be67003cf85e4e2c6e0539ec0facc45db6
NotePrin <kunprin.note@gmail.com>
2024-09-25 10:09:30 +0700

    change base url

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -2,9 +2,9 @@ import axios from "axios";
 // import { log } from "console";
 // import log from "@/log/log";
 
-// const BASE_URL = window.origin;
+const BASE_URL = window.origin;
 
-const BASE_URL = "http://cwa-alb-607898773.eu-north-1.elb.amazonaws.com";
+// const BASE_URL = "http://cwa-alb-607898773.eu-north-1.elb.amazonaws.com";
 
 // console.log("base url:", BASE_URL);
 
